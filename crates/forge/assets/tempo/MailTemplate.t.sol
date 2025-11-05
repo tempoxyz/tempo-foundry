3713a0e13d1353c530d6b47570b1a094df4e1bd0
zerosnacks <zerosnacks@protonmail.com>
2025-11-05 14:46:39 +0100

    update template

--- crates/forge/assets/tempo/MailTemplate.t.sol
+++ crates/forge/assets/tempo/MailTemplate.t.sol
@@ -3,26 +3,10 @@ pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
 import {ITIP20} from "tempo-std/interfaces/ITIP20.sol";
+import {ITIP20RolesAuth} from "tempo-std/interfaces/ITIP20RolesAuth.sol";
 import {ITIP20Factory} from "tempo-std/interfaces/ITIP20Factory.sol";
 import {StdPrecompiles} from "tempo-std/StdPrecompiles.sol";
 import {Mail} from "../src/Mail.sol";
-
-// TODO: replace this when available
-interface ITIP20RolesAuth is ITIP20 {
-    error Unauthorized();
-
-    event RoleMembershipUpdated(bytes32 indexed role, address indexed account, address indexed sender, bool hasRole);
-
-    event RoleAdminUpdated(bytes32 indexed role, bytes32 indexed newAdminRole, address indexed sender);
-
-    function grantRole(bytes32 role, address account) external;
-
-    function revokeRole(bytes32 role, address account) external;
-
-    function renounceRole(bytes32 role) external;
-
-    function setRoleAdmin(bytes32 role, bytes32 adminRole) external;
-}
 
 contract MailTest is Test {
     ITIP20Factory internal constant TIP20_FACTORY = ITIP20Factory(StdPrecompiles.TIP20_FACTORY_ADDRESS);
@@ -46,7 +30,7 @@ contract MailTest is Test {
         Mail.Attachment memory attachment =
             Mail.Attachment({amount: 100 * 10 ** token.decimals(), memo: "Invoice #1234"});
 
-        vm.prank(ALICE);
+        vm.startPrank(ALICE);
         token.approve(address(mail), attachment.amount);
 
         vm.prank(ALICE);
@@ -55,8 +39,8 @@ contract MailTest is Test {
         assertEq(token.balanceOf(BOB), attachment.amount);
     }
 
-    function testFuzz_SendMail(uint256 amount, bytes32 memo) public {
-        amount = bound(amount, 1, 10000 * 10 ** token.decimals());
+    function testFuzz_SendMail(uint256 amount, string memory message, bytes32 memo) public {
+        amount = bound(amount, 1, 100000 * 10 ** token.decimals());
 
         Mail.Attachment memory attachment = Mail.Attachment({amount: amount, memo: memo});
 
@@ -64,7 +48,7 @@ contract MailTest is Test {
         token.approve(address(mail), attachment.amount);
 
         vm.prank(ALICE);
-        mail.sendMail(BOB, "Hello, this is a fuzzed test mail.", attachment);
+        mail.sendMail(BOB, message, attachment);
 
         assertEq(token.balanceOf(BOB), attachment.amount);
     }
