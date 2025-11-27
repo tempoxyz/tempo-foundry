c362eaacde76297dc955932170df1274e3803292
zerosnacks <zerosnacks@protonmail.com>
2025-11-27 14:42:56 +0100

    fix merge conflict

--- crates/forge/tests/cli/test_cmd/mod.rs
+++ crates/forge/tests/cli/test_cmd/mod.rs
@@ -48,22 +48,8 @@ forgetest!(
         );
         }
 
-    let orig_assert = cmd.args(args).assert();
-    if orig_assert.get_output().status.success() {
-        return;
-    }
-    let stdout = orig_assert.get_output().stdout_lossy();
-    if let Some(i) = stdout.rfind("Suite result:") {
-        test_debug!("--- short stdout ---\n\n{}\n\n---", &stdout[i..]);
-    }
-
-    // Retry failed tests.
-    cmd.args(["--rerun"]);
-    let n = 3;
-    for i in 1..=n {
-        test_debug!("retrying failed tests... ({i}/{n})");
-        let assert = cmd.assert();
-        if assert.get_output().status.success() {
+        let orig_assert = cmd.args(args).assert();
+        if orig_assert.get_output().status.success() {
             return;
         }
         let stdout = orig_assert.get_output().stdout_lossy();
@@ -80,9 +66,24 @@ forgetest!(
             if assert.get_output().status.success() {
                 return;
             }
-        }
+            let stdout = orig_assert.get_output().stdout_lossy();
+            if let Some(i) = stdout.rfind("Suite result:") {
+                test_debug!("--- short stdout ---\n\n{}\n\n---", &stdout[i..]);
+            }
 
-        orig_assert.success();
+            // Retry failed tests.
+            cmd.args(["--rerun"]);
+            let n = 3;
+            for i in 1..=n {
+                test_debug!("retrying failed tests... ({i}/{n})");
+                let assert = cmd.assert();
+                if assert.get_output().status.success() {
+                    return;
+                }
+            }
+
+            orig_assert.success();
+        }
     }
 );
 
