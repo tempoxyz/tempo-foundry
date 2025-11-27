82a04b168976976dd95b15e1259bd2b85eea219d
zerosnacks <zerosnacks@protonmail.com>
2025-11-27 14:23:06 +0100

    add note

--- crates/evm/core/src/tempo.rs
+++ crates/evm/core/src/tempo.rs
@@ -38,6 +38,11 @@ impl<'a> FoundryStorageProvider<'a> {
 
 impl<'a> tempo_precompiles::storage::PrecompileStorageProvider for FoundryStorageProvider<'a> {
     fn spec(&self) -> TempoHardfork {
+        // Note: in Foundry are currently using the Prague hardfork which is unmatched with Tempo
+        // resulting in always returning the default Tempo hardfork (the oldest).
+        //
+        // Once Foundry is updated to default to Osaka this will need to be updated to return
+        // the correct hardfork again.
         TempoHardfork::Allegretto
     }
 
