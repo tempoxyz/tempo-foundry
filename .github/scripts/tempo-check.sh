b602f0f5a31603b2e8bd3e7659e201cd8d8834cc
zerosnacks <zerosnacks@protonmail.com>
2025-12-02 12:27:05 +0100

    harden smoke test

--- .github/scripts/tempo-check.sh
+++ .github/scripts/tempo-check.sh
@@ -1,18 +1,51 @@
 #!/bin/bash
-echo -e "\n=== TEMPO VERSION ==="
-cast client --rpc-url $TEMPO_RPC_URL
+
+echo -e "\n=== INIT TEMPO PROJECT ==="
 tmp_dir=$(mktemp -d)
 cd "$tmp_dir"
-echo -e "\n=== INIT TEMPO PROJECT ==="
 forge init -n tempo tempo-check
 cd tempo-check
-echo -e "\n=== FORGE TEST ==="
+
+echo -e "\n=== FORGE TEST (LOCAL) ==="
 forge test
-echo -e "\n=== FORGE SCRIPT ==="
+
+echo -e "\n=== FORGE SCRIPT (LOCAL) ==="
 forge script script/Mail.s.sol
+
+echo -e "\n=== START TEMPO FORK ==="
+export TEMPO_RPC_URL="$_TEMPO_RPC_URL"
+
+echo -e "\n=== TEMPO VERSION ==="
+cast client --rpc-url $TEMPO_RPC_URL
+
+echo -e "\n=== FORGE TEST (FORK) ==="
+forge test
+
+echo -e "\n=== FORGE SCRIPT (FORK) ==="
+forge script script/Mail.s.sol
+
 echo -e "\n=== CREATE AND FUND ADDRESS ==="
-read ADDR PK < <(cast wallet new --json | jq -r '.[0] | "\(.address) \(.private_key)"'); cast rpc tempo_fundAddress "$ADDR" --rpc-url "$TEMPO_RPC_URL"; printf "\naddress: %s\nprivate_key: %s\n" "$ADDR" "$PK"
-echo -e "\n=== FORGE SCRIPT DEPLOY AND VERIFY ==="
-forge script script/Mail.s.sol --private-key $PK --broadcast --verify
-echo -e "\n=== FORGE CREATE DEPLOY AND VERIFY ==="
-forge create src/Mail.sol:Mail --rpc-url $TEMPO_RPC_URL --private-key $PK --broadcast --verify --constructor-args 0x20c0000000000000000000000000000000000000
+read ADDR PK < <(cast wallet new --json | jq -r '.[0] | "\(.address) \(.private_key)"')
+
+for i in {1..100}; do
+  OUT=$(cast rpc tempo_fundAddress "$ADDR" --rpc-url "$TEMPO_RPC_URL" 2>&1)
+
+  if echo "$OUT" | jq -e 'arrays' >/dev/null 2>&1; then
+    echo "$OUT" | jq
+    break
+  fi
+
+  echo "[$i] $OUT"
+  sleep 0.2
+done
+
+printf "\naddress: %s\nprivate_key: %s\n" "$ADDR" "$PK"
+
+echo -e "\n=== WAIT FOR BLOCKS TO MINE ==="
+sleep 5
+
+echo -e "\n=== FORGE SCRIPT DEPLOY ==="
+forge script script/Mail.s.sol --private-key $PK --broadcast
+
+echo -e "\n=== FORGE CREATE DEPLOY ==="
+forge create src/Mail.sol:Mail --rpc-url $TEMPO_RPC_URL --private-key $PK --broadcast --constructor-args 0x20c0000000000000000000000000000000000000
