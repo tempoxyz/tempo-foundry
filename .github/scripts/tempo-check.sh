4dd93f9f085ddc3b8141834df5bf09f1c64094d2
grandizzy <grandizzy.the.egg@gmail.com>
2025-12-05 11:08:41 +0200

    bump dep, fail CI when failure

--- .github/scripts/tempo-check.sh
+++ .github/scripts/tempo-check.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 
 echo -e "\n=== INIT TEMPO PROJECT ==="
 tmp_dir=$(mktemp -d)
@@ -54,33 +55,24 @@ forge script --fee-token 1 script/Mail.s.sol --private-key $PK --rpc-url $TEMPO_
 
 echo -e "\n=== FORGE SCRIPT DEPLOY WITH FEE TOKEN ==="
 forge script --fee-token 2 script/Mail.s.sol --private-key $PK --rpc-url $TEMPO_RPC_URL --broadcast ${VERIFY_ARGS[@]}
-forge script --fee-token 3 script/Mail.s.sol --private-key $PK --rpc-url $TEMPO_RPC_URL --broadcast ${VERIFY_ARGS[@]}
 
 echo -e "\n=== FORGE CREATE DEPLOY ==="
-forge create src/Mail.sol:Mail --private-key $PK --rpc-url $TEMPO_RPC_URL --broadcast ${VERIFY_ARGS[@]} --constructor-args 0x20c0000000000000000000000000000000000000
+forge create --fee-token 1 src/Mail.sol:Mail --private-key $PK --rpc-url $TEMPO_RPC_URL --broadcast ${VERIFY_ARGS[@]} --constructor-args 0x20c0000000000000000000000000000000000000
 
 echo -e "\n=== FORGE CREATE DEPLOY WITH FEE TOKEN ==="
 forge create --fee-token 2 src/Mail.sol:Mail --private-key $PK --rpc-url $TEMPO_RPC_URL --broadcast ${VERIFY_ARGS[@]} --constructor-args 0x20c0000000000000000000000000000000000000
-forge create --fee-token 3 src/Mail.sol:Mail --private-key $PK --rpc-url $TEMPO_RPC_URL --broadcast ${VERIFY_ARGS[@]} --constructor-args 0x20c0000000000000000000000000000000000000
 
 echo -e "\n=== CAST ERC20 TRANSFER ==="
 cast erc20 transfer 0x20c0000000000000000000000000000000000002 0x4ef5DFf69C1514f4Dbf85aA4F9D95F804F64275F 123456 --rpc-url $TEMPO_RPC_URL --private-key $PK
 
 echo -e "\n=== CAST ERC20 TRANSFER WITH FEE TOKEN ==="
 cast erc20 transfer --fee-token 2 0x20c0000000000000000000000000000000000002 0x4ef5DFf69C1514f4Dbf85aA4F9D95F804F64275F 123456 --rpc-url $TEMPO_RPC_URL --private-key $PK
-cast erc20 transfer --fee-token 3 0x20c0000000000000000000000000000000000003 0x4ef5DFf69C1514f4Dbf85aA4F9D95F804F64275F 123456 --rpc-url $TEMPO_RPC_URL --private-key $PK
-
-echo -e "\n=== CAST ERC20 APPROVE ==="
-cast erc20 approve 0x20c0000000000000000000000000000000000002 0x4ef5DFf69C1514f4Dbf85aA4F9D95F804F64275F 123456 --rpc-url $TEMPO_RPC_URL --private-key $PK
 
 echo -e "\n=== CAST ERC20 APPROVE WITH FEE TOKEN ==="
 cast erc20 approve --fee-token 2 0x20c0000000000000000000000000000000000002 0x4ef5DFf69C1514f4Dbf85aA4F9D95F804F64275F 123456 --rpc-url $TEMPO_RPC_URL --private-key $PK
-cast erc20 approve --fee-token 3 0x20c0000000000000000000000000000000000003 0x4ef5DFf69C1514f4Dbf85aA4F9D95F804F64275F 123456 --rpc-url $TEMPO_RPC_URL --private-key $PK
 
 echo -e "\n=== CAST SEND WITH FEE TOKEN ==="
-cast send --fee-token 1 --rpc-url $TEMPO_RPC_URL 0x86A2EE8FAf9A840F7a2c64CA3d51209F9A02081D 'increment()' --private-key $PK
 cast send --fee-token 2 --rpc-url $TEMPO_RPC_URL 0x86A2EE8FAf9A840F7a2c64CA3d51209F9A02081D 'increment()' --private-key $PK
-cast send --fee-token 3 --rpc-url $TEMPO_RPC_URL 0x86A2EE8FAf9A840F7a2c64CA3d51209F9A02081D 'increment()' --private-key $PK
 
 echo -e "\n=== CAST MKTX WITH FEE TOKEN ==="
 cast mktx --fee-token 2 --rpc-url $TEMPO_RPC_URL 0x86A2EE8FAf9A840F7a2c64CA3d51209F9A02081D 'increment()' --private-key $PK
