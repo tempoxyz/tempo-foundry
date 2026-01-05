5faa16e6c89526f6ede8d8c22c285283abc73031
zerosnacks <zerosnacks@protonmail.com>
2026-01-05 10:20:38 +0100

    install from master branch for tempo compatibility

--- crates/forge/src/cmd/init.rs
+++ crates/forge/src/cmd/init.rs
@@ -263,7 +263,7 @@ impl InitArgs {
                         sh_warn!("\"lib/tempo-std\" already exists, skipping install...")?;
                         self.install.install(&mut config, vec![]).await?;
                     } else {
-                        let dep = "https://github.com/tempoxyz/tempo-std".parse()?;
+                        let dep = "https://github.com/tempoxyz/tempo-std@master".parse()?;
                         self.install.install(&mut config, vec![dep]).await?;
                     }
                 }
