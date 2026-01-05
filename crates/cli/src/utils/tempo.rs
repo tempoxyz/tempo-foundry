eb089407047a718bc1ff8640635e0edc87fab0d2
grandizzy <grandizzy.the.egg@gmail.com>
2026-01-05 17:33:40 +0200

    bump + Add helper back

--- crates/cli/src/utils/tempo.rs
+++ crates/cli/src/utils/tempo.rs
@@ -1,4 +1,4 @@
-use alloy_primitives::Address;
+use alloy_primitives::{Address, hex};
 use alloy_provider::network::EthereumWallet;
 use foundry_common::provider::tempo::{
     TempoProviderBuilder, TempoRetryProvider, TempoRetryProviderWithSigner,
@@ -47,6 +47,13 @@ pub fn get_tempo_provider_builder(config: &Config) -> eyre::Result<TempoProvider
 }
 
 /// Parses a fee token address.
-pub fn parse_fee_token_address(address: &str) -> eyre::Result<Address> {
-    Ok(Address::from_str(address)?)
+pub fn parse_fee_token_address(address_or_id: &str) -> eyre::Result<Address> {
+    Address::from_str(address_or_id).or_else(|_| Ok(token_id_to_address(address_or_id.parse()?)))
+}
+
+fn token_id_to_address(token_id: u64) -> Address {
+    let mut address_bytes = [0u8; 20];
+    address_bytes[..12].copy_from_slice(&hex!("20C000000000000000000000"));
+    address_bytes[12..20].copy_from_slice(&token_id.to_be_bytes());
+    Address::from(address_bytes)
 }
